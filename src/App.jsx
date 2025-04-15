0e30b1064f0ee0d24af63803bbb5bf1ffa347afa
LivinkumarS <001liv100@ggmail.com>
2025-04-15 13:52:03 +0530

    Add Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="overflow-x-hidden">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/sign-up" element={<SignUp/>} />
         </Routes>
